6389811a1b2739a603046b3c1031af0939e58016
tleTerror <121960613+tleTerror@users.noreply.github.com>
2023-06-05 23:08:35 +0530

    added script.js, style.css,vanilla-tilt.js

---
+++ script.js
@@ -0,0 +1,25 @@
+//hamburger
+const navSlide = () => { // () => it allows us to write shorter function
+    const hamburger = document.querySelector(".hamburger");
+    const navbar = document.querySelector(".nav-bar");
+    const navLinks = document.querySelectorAll(".nav-bar li");
+
+    hamburger.onclick = () => {
+       
+    navbar.classList.toggle("nav-active");
+        
+     //Animation links
+    navLinks.forEach((link, index) => {
+       if (link.style.animation) {
+           link.style.animation = "";
+       } else {
+           link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
+          }
+       });
+     //hamburger animation
+    hamburger.classList.toggle("toggle");
+   }
+    
+   }
+
+window.onload = () => navSlide();
